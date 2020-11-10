KleirRampage45/redot-engine

Prefer the highlight version of the "GuiTabIcon" icon for buttons, and make their width/height equal

editor/editor_audio_buses.cpp
@@ -84,7 +84,7 @@ void EditorAudioBus::_notification(int p_what) {
 			bypass->set_icon(get_theme_icon("AudioBusBypass", "EditorIcons"));
 			bypass->add_theme_color_override("icon_color_pressed", bypass_color);
 
-			bus_options->set_icon(get_theme_icon("GuiTabMenu", "EditorIcons"));
+			bus_options->set_icon(get_theme_icon("GuiTabMenuHl", "EditorIcons"));
 
 			update_bus();
 			set_process(true);
@@ -172,7 +172,7 @@ void EditorAudioBus::_notification(int p_what) {
 			mute->set_icon(get_theme_icon("AudioBusMute", "EditorIcons"));
 			bypass->set_icon(get_theme_icon("AudioBusBypass", "EditorIcons"));
 
-			bus_options->set_icon(get_theme_icon("GuiTabMenu", "EditorIcons"));
+			bus_options->set_icon(get_theme_icon("GuiTabMenuHl", "EditorIcons"));
 		} break;
 		case NOTIFICATION_MOUSE_EXIT:
 		case NOTIFICATION_DRAG_END: {

editor/plugins/animation_player_editor_plugin.cpp
@@ -117,7 +117,7 @@ void AnimationPlayerEditor::_notification(int p_what) {
 			stop->set_icon(get_theme_icon("Stop", "EditorIcons"));
 
 			onion_toggle->set_icon(get_theme_icon("Onion", "EditorIcons"));
-			onion_skinning->set_icon(get_theme_icon("GuiTabMenu", "EditorIcons"));
+			onion_skinning->set_icon(get_theme_icon("GuiTabMenuHl", "EditorIcons"));
 
 			pin->set_icon(get_theme_icon("Pin", "EditorIcons"));
 

editor/plugins/canvas_item_editor_plugin.cpp
@@ -3982,7 +3982,7 @@ void CanvasItemEditor::_notification(int p_what) {
 		rotate_button->set_icon(get_theme_icon("ToolRotate", "EditorIcons"));
 		smart_snap_button->set_icon(get_theme_icon("Snap", "EditorIcons"));
 		grid_snap_button->set_icon(get_theme_icon("SnapGrid", "EditorIcons"));
-		snap_config_menu->set_icon(get_theme_icon("GuiTabMenu", "EditorIcons"));
+		snap_config_menu->set_icon(get_theme_icon("GuiTabMenuHl", "EditorIcons"));
 		skeleton_menu->set_icon(get_theme_icon("Bone", "EditorIcons"));
 		override_camera_button->set_icon(get_theme_icon("Camera2D", "EditorIcons"));
 		pan_button->set_icon(get_theme_icon("ToolPan", "EditorIcons"));
@@ -3999,7 +3999,7 @@ void CanvasItemEditor::_notification(int p_what) {
 		key_scale_button->set_icon(get_theme_icon("KeyScale", "EditorIcons"));
 		key_insert_button->set_icon(get_theme_icon("Key", "EditorIcons"));
 		key_auto_insert_button->set_icon(get_theme_icon("AutoKey", "EditorIcons"));
-		animation_menu->set_icon(get_theme_icon("GuiTabMenu", "EditorIcons"));
+		animation_menu->set_icon(get_theme_icon("GuiTabMenuHl", "EditorIcons"));
 
 		zoom_minus->set_icon(get_theme_icon("ZoomLess", "EditorIcons"));
 		zoom_plus->set_icon(get_theme_icon("ZoomMore", "EditorIcons"));

Calinou/godot

(cherry picked from commit 26381265b88d02412593d5a786772544e6a1ac09)

editor/editor_audio_buses.cpp
@@ -89,7 +89,7 @@ void EditorAudioBus::_notification(int p_what) {
 			bypass->set_icon(get_icon("AudioBusBypass", "EditorIcons"));
 			bypass->add_color_override("icon_color_pressed", bypass_color);
 
-			bus_options->set_icon(get_icon("GuiTabMenu", "EditorIcons"));
+			bus_options->set_icon(get_icon("GuiTabMenuHl", "EditorIcons"));
 
 			update_bus();
 			set_process(true);
@@ -181,7 +181,7 @@ void EditorAudioBus::_notification(int p_what) {
 			mute->set_icon(get_icon("AudioBusMute", "EditorIcons"));
 			bypass->set_icon(get_icon("AudioBusBypass", "EditorIcons"));
 
-			bus_options->set_icon(get_icon("GuiTabMenu", "EditorIcons"));
+			bus_options->set_icon(get_icon("GuiTabMenuHl", "EditorIcons"));
 		} break;
 		case NOTIFICATION_MOUSE_EXIT:
 		case NOTIFICATION_DRAG_END: {

editor/plugins/animation_player_editor_plugin.cpp
@@ -126,7 +126,7 @@ void AnimationPlayerEditor::_notification(int p_what) {
 			stop->set_icon(get_icon("Stop", "EditorIcons"));
 
 			onion_toggle->set_icon(get_icon("Onion", "EditorIcons"));
-			onion_skinning->set_icon(get_icon("GuiTabMenu", "EditorIcons"));
+			onion_skinning->set_icon(get_icon("GuiTabMenuHl", "EditorIcons"));
 
 			pin->set_icon(get_icon("Pin", "EditorIcons"));
 

editor/plugins/canvas_item_editor_plugin.cpp
@@ -3861,7 +3861,7 @@ void CanvasItemEditor::_notification(int p_what) {
 		rotate_button->set_icon(get_icon("ToolRotate", "EditorIcons"));
 		smart_snap_button->set_icon(get_icon("Snap", "EditorIcons"));
 		grid_snap_button->set_icon(get_icon("SnapGrid", "EditorIcons"));
-		snap_config_menu->set_icon(get_icon("GuiTabMenu", "EditorIcons"));
+		snap_config_menu->set_icon(get_icon("GuiTabMenuHl", "EditorIcons"));
 		skeleton_menu->set_icon(get_icon("Bone", "EditorIcons"));
 		override_camera_button->set_icon(get_icon("Camera2D", "EditorIcons"));
 		pan_button->set_icon(get_icon("ToolPan", "EditorIcons"));
@@ -3878,7 +3878,7 @@ void CanvasItemEditor::_notification(int p_what) {
 		key_scale_button->set_icon(get_icon("KeyScale", "EditorIcons"));
 		key_insert_button->set_icon(get_icon("Key", "EditorIcons"));
 		key_auto_insert_button->set_icon(get_icon("AutoKey", "EditorIcons"));
-		animation_menu->set_icon(get_icon("GuiTabMenu", "EditorIcons"));
+		animation_menu->set_icon(get_icon("GuiTabMenuHl", "EditorIcons"));
 
 		zoom_minus->set_icon(get_icon("ZoomLess", "EditorIcons"));
 		zoom_plus->set_icon(get_icon("ZoomMore", "EditorIcons"));