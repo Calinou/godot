31bc9839e73c4da77939068024fb2a2a6ff1512e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-05-16 01:36:01 +0200

    [WIP] Improve the editor native shader source visualizer
    - Use CodeEdit to enable features such as line numbers and minimap.
    - Enable syntax highlighting.
    - Use a fixed-width font.
    - Use the script editor settings.
    
    TODO:
    
    - Improve performance by creating CodeEdit instances only when selecting
      tabs.
    - Fix comment highlighting color looking identical to the rest of the text
      (probably due to read-only alpha setting?).
    - Fix bright highlight when the CodeEdit is focused.

--- editor/editor_native_shader_source_visualizer.cpp
+++ editor/editor_native_shader_source_visualizer.cpp
@@ -30,7 +30,42 @@
 
 #include "editor_native_shader_source_visualizer.h"
 
+#include "editor/code_editor.h"
+#include "editor/editor_scale.h"
+#include "editor/editor_settings.h"
 #include "scene/gui/text_edit.h"
+
+void EditorNativeShaderSourceVisualizer::_load_theme_settings() {
+	syntax_highlighter->set_number_color(EDITOR_GET("text_editor/highlighting/number_color"));
+	syntax_highlighter->set_symbol_color(EDITOR_GET("text_editor/highlighting/symbol_color"));
+	syntax_highlighter->set_function_color(EDITOR_GET("text_editor/highlighting/function_color"));
+	syntax_highlighter->set_member_variable_color(EDITOR_GET("text_editor/highlighting/member_variable_color"));
+
+	syntax_highlighter->clear_keyword_colors();
+
+	List<String> keywords;
+	ShaderLanguage::get_keyword_list(&keywords);
+	const Color keyword_color = EDITOR_GET("text_editor/highlighting/keyword_color");
+	const Color control_flow_keyword_color = EDITOR_GET("text_editor/highlighting/control_flow_keyword_color");
+
+	for (List<String>::Element *E = keywords.front(); E; E = E->next()) {
+		if (ShaderLanguage::is_control_flow_keyword(E->get())) {
+			syntax_highlighter->add_keyword_color(E->get(), control_flow_keyword_color);
+		} else {
+			syntax_highlighter->add_keyword_color(E->get(), keyword_color);
+		}
+	}
+
+	// Colorize comments.
+	const Color comment_color = EDITOR_GET("text_editor/highlighting/comment_color");
+	syntax_highlighter->clear_color_regions();
+	syntax_highlighter->add_color_region("/*", "*/", comment_color, false);
+	syntax_highlighter->add_color_region("//", "", comment_color, true);
+
+	// Colorize preprocessor statements.
+	const Color string_color = EDITOR_GET("text_editor/highlighting/string_color");
+	syntax_highlighter->add_color_region("#", "", string_color, true);
+}
 
 void EditorNativeShaderSourceVisualizer::_inspect_shader(RID p_shader) {
 	if (versions) {
@@ -39,6 +74,8 @@ void EditorNativeShaderSourceVisualizer::_inspect_shader(RID p_shader) {
 	}
 
 	RS::ShaderNativeSourceCode nsc = RS::get_singleton()->shader_get_native_source_code(p_shader);
+
+	_load_theme_settings();
 
 	versions = memnew(TabContainer);
 	versions->set_v_size_flags(Control::SIZE_EXPAND_FILL);
@@ -50,13 +87,41 @@ void EditorNativeShaderSourceVisualizer::_inspect_shader(RID p_shader) {
 		vtab->set_h_size_flags(Control::SIZE_EXPAND_FILL);
 		versions->add_child(vtab);
 		for (int j = 0; j < nsc.versions[i].stages.size(); j++) {
-			TextEdit *vtext = memnew(TextEdit);
-			vtext->set_readonly(true);
-			vtext->set_name(nsc.versions[i].stages[j].name);
-			vtext->set_text(nsc.versions[i].stages[j].code);
-			vtext->set_v_size_flags(Control::SIZE_EXPAND_FILL);
-			vtext->set_h_size_flags(Control::SIZE_EXPAND_FILL);
-			vtab->add_child(vtext);
+			CodeEdit *code_edit = memnew(CodeEdit);
+			code_edit->set_readonly(true);
+			code_edit->set_syntax_highlighter(syntax_highlighter);
+			code_edit->add_theme_font_override("font", get_theme_font("source", "EditorFonts"));
+			code_edit->add_theme_font_size_override("font_size", get_theme_font_size("source_size", "EditorFonts"));
+			code_edit->add_theme_constant_override("line_spacing", EDITOR_DEF("text_editor/theme/line_spacing", 6));
+
+			// Set settings in a way similar to `editor/code_editor.cpp`.
+			// Some settings don't make sense to be changed in a read-only context,
+			// so these aren't set here.
+			code_edit->set_highlight_all_occurrences(EditorSettings::get_singleton()->get("text_editor/highlighting/highlight_all_occurrences"));
+			code_edit->set_indent_using_spaces(EditorSettings::get_singleton()->get("text_editor/indent/type"));
+			code_edit->set_indent_size(EditorSettings::get_singleton()->get("text_editor/indent/size"));
+			code_edit->set_auto_indent(EditorSettings::get_singleton()->get("text_editor/indent/auto_indent"));
+			code_edit->set_draw_tabs(EditorSettings::get_singleton()->get("text_editor/indent/draw_tabs"));
+			code_edit->set_draw_spaces(EditorSettings::get_singleton()->get("text_editor/indent/draw_spaces"));
+			code_edit->set_smooth_scroll_enabled(EditorSettings::get_singleton()->get("text_editor/navigation/smooth_scrolling"));
+			code_edit->set_v_scroll_speed(EditorSettings::get_singleton()->get("text_editor/navigation/v_scroll_speed"));
+			code_edit->set_draw_minimap(EditorSettings::get_singleton()->get("text_editor/navigation/show_minimap"));
+			code_edit->set_minimap_width((int)EditorSettings::get_singleton()->get("text_editor/navigation/minimap_width") * EDSCALE);
+			code_edit->set_draw_line_numbers(EditorSettings::get_singleton()->get("text_editor/appearance/show_line_numbers"));
+			code_edit->set_line_numbers_zero_padded(EditorSettings::get_singleton()->get("text_editor/appearance/line_numbers_zero_padded"));
+			code_edit->set_hiding_enabled(EditorSettings::get_singleton()->get("text_editor/appearance/code_folding"));
+			code_edit->set_draw_fold_gutter(EditorSettings::get_singleton()->get("text_editor/appearance/code_folding"));
+			code_edit->set_wrap_enabled(EditorSettings::get_singleton()->get("text_editor/appearance/word_wrap"));
+			code_edit->set_scroll_pass_end_of_file(EditorSettings::get_singleton()->get("text_editor/cursor/scroll_past_end_of_file"));
+			code_edit->cursor_set_block_mode(EditorSettings::get_singleton()->get("text_editor/cursor/block_caret"));
+			code_edit->cursor_set_blink_enabled(EditorSettings::get_singleton()->get("text_editor/cursor/caret_blink"));
+			code_edit->cursor_set_blink_speed(EditorSettings::get_singleton()->get("text_editor/cursor/caret_blink_speed"));
+
+			code_edit->set_name(nsc.versions[i].stages[j].name);
+			code_edit->set_text(nsc.versions[i].stages[j].code);
+			code_edit->set_v_size_flags(Control::SIZE_EXPAND_FILL);
+			code_edit->set_h_size_flags(Control::SIZE_EXPAND_FILL);
+			vtab->add_child(code_edit);
 		}
 	}
 	add_child(versions);
@@ -66,7 +131,10 @@ void EditorNativeShaderSourceVisualizer::_inspect_shader(RID p_shader) {
 void EditorNativeShaderSourceVisualizer::_bind_methods() {
 	ClassDB::bind_method("_inspect_shader", &EditorNativeShaderSourceVisualizer::_inspect_shader);
 }
+
 EditorNativeShaderSourceVisualizer::EditorNativeShaderSourceVisualizer() {
+	syntax_highlighter.instance();
+
 	add_to_group("_native_shader_source_visualizer");
 	set_title(TTR("Native Shader Source Inspector"));
 }
