67d1c27c6c9ca8573c8ef7e1745e2f36c67ea975
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-08-21 05:40:20 +0200

    [WIP] Draw a direct specular light lobe in directional LightmapGI rendering
    This is based on the dominant light direction, with an automatic roughness term.
    
    TODO:
    
    - Fix lightmap direction based on SH coefficients.
    - Check if the automatic roughness term is correct (with regards to linear/sqrt).
    - Make this optional behind a project setting if it turns out to be costly
      on mobile/integrated graphics.

--- drivers/gles3/shaders/scene.glsl
+++ drivers/gles3/shaders/scene.glsl
@@ -1248,7 +1248,8 @@ vec3 F0(float metallic, float specular, vec3 albedo) {
 #ifndef MODE_RENDER_DEPTH
 
 #ifndef USE_VERTEX_LIGHTING
-#if !defined(DISABLE_LIGHT_DIRECTIONAL) || !defined(DISABLE_LIGHT_OMNI) || !defined(DISABLE_LIGHT_SPOT) || defined(USE_ADDITIVE_LIGHTING)
+// `light_compute()` is needed for `USE_SH_LIGHTMAP` (fake specular lobes with spherical harmonics lightmaps).
+#if !defined(DISABLE_LIGHT_DIRECTIONAL) || !defined(DISABLE_LIGHT_OMNI) || !defined(DISABLE_LIGHT_SPOT) || defined(USE_ADDITIVE_LIGHTING) || defined(USE_SH_LIGHTMAP)
 
 float D_GGX(float cos_theta_m, float alpha) {
 	float a = cos_theta_m * alpha;
@@ -2094,10 +2095,39 @@ void main() {
 
 		vec3 n = normalize(lightmap_normal_xform * normal);
 
-		ambient_light += lm_light_l0 * lightmap_exposure_normalization;
-		ambient_light += lm_light_l1n1 * n.y * (lm_light_l0 * lightmap_exposure_normalization * 4.0);
-		ambient_light += lm_light_l1_0 * n.z * (lm_light_l0 * lightmap_exposure_normalization * 4.0);
-		ambient_light += lm_light_l1p1 * n.x * (lm_light_l0 * lightmap_exposure_normalization * 4.0);
+		vec3 sh_light = vec3(0.0);
+		sh_light += lm_light_l0 * lightmap_exposure_normalization;
+		sh_light += lm_light_l1n1 * n.y * (lm_light_l0 * lightmap_exposure_normalization * 4.0);
+		sh_light += lm_light_l1_0 * n.z * (lm_light_l0 * lightmap_exposure_normalization * 4.0);
+		sh_light += lm_light_l1p1 * n.x * (lm_light_l0 * lightmap_exposure_normalization * 4.0);
+		ambient_light += sh_light;
+
+		// Fake specular light to create some direct light specular lobes for directional lightmaps.
+		// https://media.contentapi.ea.com/content/dam/eacom/frostbite/files/gdc2018-precomputedgiobalilluminationinfrostbite.pdf (slides 66-71)
+		vec3 lightmap_direction = (lm_light_l1n1 + lm_light_l1_0 + lm_light_l1p1) * 0.333333; // FIXME: Determine direction correctly.
+		float lightmap_direction_length = length(lightmap_direction); // Value in range [0..1].
+		vec3 L = lightmap_direction / lightmap_direction_length;
+		float adjusted_roughness = 1.0 - ((1.0 - roughness) * sqrt(lightmap_direction_length));
+
+		vec3 f0 = F0(metallic, specular, albedo);
+
+		// Discard diffuse light from this fake light, as we're only interested in its specular light output.
+		vec3 diffuse_light_discarded = diffuse_light;
+		light_compute(normal, L, view, 0.0, sh_light, false, 1.0, f0, roughness, metallic, 50.0 /* Boosted specular amount for testing */, albedo, alpha, screen_uv,
+#ifdef LIGHT_BACKLIGHT_USED
+				backlight,
+#endif
+#ifdef LIGHT_RIM_USED
+				rim, rim_tint,
+#endif
+#ifdef LIGHT_CLEARCOAT_USED
+				clearcoat, clearcoat_roughness, normalize(normal_interp),
+#endif
+#ifdef LIGHT_ANISOTROPY_USED
+				binormal, tangent, anisotropy,
+#endif
+				diffuse_light_discarded,
+				specular_light);
 #else
 #ifdef LIGHTMAP_BICUBIC_FILTER
 		ambient_light += textureArray_bicubic(lightmap_textures, uvw, lightmap_texture_size).rgb * lightmap_exposure_normalization;
